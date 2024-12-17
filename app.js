b07b02a7324ef58f9cd2a2edecce951e2ae980ca
ayanhabib8 <ayanhabib265@gmail.com>
2024-12-18 00:27:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// Chapter (1)
+
+// Question (1)
+alert("")
+
+// Question  (2)
+
+alert("wellcome to java script")
+
+// Question (3)
+
+alert("wellcome  \nto \n javascript")
+
+// Question (4)
+
+alert("wellcome to js")
+
+// Question (5)
+
+console.log("Hellow")
+
